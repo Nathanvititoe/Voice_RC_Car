7c89a16a262d5465168cd2f3196c40ff13efdcd4
Nathanvititoe <nathanvititoe@gmail.com>
2025-07-24 20:14:15 -0400

    Removed unnecessary header

--- micro_speech_model_data.h
+++
@@ -1,8 +0,0 @@
-// micro_speech_model_data.h - Placeholder model header
-#ifndef MICRO_SPEECH_MODEL_DATA_H_
-#define MICRO_SPEECH_MODEL_DATA_H_
-
-extern const unsigned char g_micro_speech_model_data[];
-extern const int g_micro_speech_model_data_len;
-
-#endif  // MICRO_SPEECH_MODEL_DATA_H_
