a6a9649536408d157e8bed770559d7e077767aaa
Nathanvititoe <nathanvititoe@gmail.com>
2025-07-25 18:05:42 -0400

    fixed arduino file

--- arduino/rc_car_voice.ino
+++ arduino/rc_car_voice.ino
@@ -1,8 +1,9 @@
 
 // Nathan Vititoe - Voice Controlled RC Car
 
-// Prototype code for implementing TF Lite micro speech for RC Car control
-// using voice recognition
+// Prototype code for connecting to arduino from PC using BLE and
+// using the Vosk Voice Recognition ML model on the PC for voice recognition RC Car control
+
 // available commands are "stop", "go", "left", "right", "down"
 
 // stop makes the car stop
@@ -11,17 +12,11 @@
 // right makes the car turn right
 // down makes the car go in reverse
 
-// TODO: find library that supports mbed_nano architecture
-#include <MicroTFLite.h>
-#include "micro_speech_model_data.h"
+#include <ArduinoBLE.h>
 
-//  define tensor size
-constexpr int kTensorArenaSize = 10 * 1024;
-uint8_t tensor_arena[kTensorArenaSize];
-
-// init input/output
-int16_t* input;
-int8_t* output;
+// init BLE control
+BLEService controlService("180D");                // Custom service UUID
+BLECharacteristic command("2A56", BLEWrite, 20);  // Command up to 20 bytes (as string)
 
 // Motor pins
 const int left_ena_1 = 6;
@@ -38,14 +33,24 @@ const int right_front_pos = 4;
 const int right_rear_pos = 5;
 const int right_rear_neg = 7;
 
-#define AUDIO_PIN A0
-const int sampleWindow = 62;  // 62 microsecond samples
-const int numSamples = 512;
-
 void setup() {
   Serial.begin(115200);
+  while (!Serial)
+    ;
+  setup_motors();
+  setup_BLE();
+  Serial.println("Setup Complete.");
+}
 
-  // Set motor pins to OUTPUT
+void loop() {
+  BLE.poll();  // poll the BLE connection for incoming commands
+}
+
+/*
+* inititalizes all motor pins as output
+*/
+void setup_motors() {
+  // set motor pins to output
   pinMode(left_ena_1, OUTPUT);
   pinMode(left_ena_3, OUTPUT);
   pinMode(left_front_neg, OUTPUT);
@@ -59,88 +64,108 @@ void setup() {
   pinMode(right_front_pos, OUTPUT);
   pinMode(right_rear_pos, OUTPUT);
   pinMode(right_rear_neg, OUTPUT);
-
-  // init model, interpreter, and error reporter
-  ModelInit(g_micro_speech_model_data, tensor_arena, kTensorArenaSize);
-
-  Serial.println("Setup Complete.");
 }
 
-void loop() {
-  // fill model input tensor with audio samples
-  constexpr int sampleCount = 512;
-  static int16_t micBuffer[sampleCount];
-
-  for (int i = 0; i < sampleCount; i++) {
-    int16_t sample = analogRead(AUDIO_PIN) - 512;
-    float normalized = sample / 512.0f;  // normalize values
-    ModelSetInput(normalized, i);
-    delayMicroseconds(sampleWindow);
+/*
+* configure the BLE service
+*/
+void setup_BLE() {
+  // ensure BLE setup is successful
+  if (!BLE.begin()) {
+    Serial.println("BLE failed to start.");
+    while (1)
+      ;
   }
 
-  // throw error if model cant run an inference
-  if (!ModelRunInference()) {
-    Serial.println("Inference failed.");
-    return;
-  }
+  BLE.setLocalName("Voice_RC");
+  BLE.setAdvertisedService(controlService);
+  controlService.addCharacteristic(command);
+  BLE.addService(controlService);
+  BLE.advertise();
 
-  // TODO: fix this
-  // assign confidence scores
-  int max_score = -128;
-  int max_index = -1;
-  constexpr int outputSize = 12;
-  for (int i = 0; i < outputSize; i++) {
-    int8_t score = (int8_t)ModelGetOutput(i, true);  // get quantized output
-    if (score > max_score) {
-      max_score = score;
-      max_index = i;
-    }
-  }
+  command.setEventHandler(BLEWritten, handleCommand);
 
-  // if confidence is above 50, map it to a word
-  if (max_score > 50) {
-    const char* cmd = commandLabel(max_index);
-    Serial.print("Heard: ");
-    Serial.println(cmd);
-    handleCommand(cmd);
-  }
+  Serial.println("BLE Ready");
 }
 
-// map voice commands
-const char* commandLabel(int index) {
-  switch (index) {
-    case 2: return "stop";
-    case 3: return "go";
-    case 4: return "left";
-    case 5: return "right";
-    case 10: return "down";
-    default: return "unknown";
+/*
+* Interpret incoming BLE commands and call their related functions
+* @param central the central device (PC)
+* @param command the command that triggered this function
+*/
+void handleCommand(BLEDevice central, BLECharacteristic command) {
+  if (!command.written()) return;  // if no commands, do nothing
+
+  String cmd = String((const char*)command.value());  // get the command
+  cmd.toUpperCase(); // ensure its uppercase
+  
+  // print the received command
+  Serial.print("Received BLE command: ");
+  Serial.println(cmd);
+
+  // map incoming commands to movement functions
+  if (cmd == "FWD") go_forward();
+  else if (cmd == "STOP") stop_motors();
+  else if (cmd == "LEFT") turn_left();
+  else if (cmd == "RIGHT") turn_right();
+  else if (cmd == "REV") reverse();
+  else if (cmd == "FAST") set_speed(200);
+  else if (cmd == "SLOW") set_speed(100);
+  else {
+    Serial.println("Unknown command received.");
+    stop_motors();
   }
 }
 
-// handle each voice command
-void handleCommand(const char* cmd) {
-  if (strcmp(cmd, "go") == 0) {
-    go_forward();
-  } else if (strcmp(cmd, "down") == 0) {
-    reverse();
-  } else if (strcmp(cmd, "left") == 0) {
-    turn_left();
-  } else if (strcmp(cmd, "right") == 0) {
-    turn_right();
-  } else if (strcmp(cmd, "stop") == 0) {
-    stop();
-  } else {
-    return;
+// ========= MOVEMENT FUNCTIONS BELOW ================
+int currentSpeed = 100;  // default 1/2 speed
+
+/*
+* step up or down speed incrementally 
+* @param enaPin - enablePin to change speed on
+* @param startValue - starting speed
+* @param endValue - ending speed
+* @param delay - how long between steps in ms
+*/
+void increment_speed(int enaPin, int startValue, int endValue, int delayMs = 10) {
+  // determine if we are stepping up or down
+  int step = (startValue < endValue) ? 1 : -1;
+
+  for (int value = startValue; value != endValue; value += step) {
+    analogWrite(enaPin, value);
+    delay(delayMs);
   }
+  analogWrite(enaPin, endValue);
 }
 
-// move the car forward
+/*
+* Change the speed of the car
+* @param speed - speed to change the car to
+*/
+void set_speed(int newSpeed) {
+  // ensure new speed stays in range, and always below 255 to prevent motor burnout
+  newSpeed = constrain(newSpeed, 0, 200); 
+  if (newSpeed == currentSpeed) return;
+
+  Serial.print("Changing speed from ");
+  Serial.print(currentSpeed);
+  Serial.print(" to ");
+  Serial.println(newSpeed);
+
+  // ramp speed on each enable pin
+  increment_speed(left_ena_1, currentSpeed, newSpeed);
+  increment_speed(left_ena_3, currentSpeed, newSpeed);
+  increment_speed(right_ena_1, currentSpeed, newSpeed);
+  increment_speed(right_ena_3, currentSpeed, newSpeed);
+
+  currentSpeed = newSpeed;
+}
+
+/*
+* move the car forward
+*/
 void go_forward() {
-  // set left speed to 100 through enable pins
-  analogWrite(left_ena_1, 100);
-  analogWrite(left_ena_3, 100);
-
+  // using the current speed
   // move left wheels forward
   digitalWrite(left_front_pos, HIGH);
   digitalWrite(left_rear_neg, HIGH);
@@ -148,10 +173,6 @@ void go_forward() {
   // ensure left reverse is off
   digitalWrite(left_front_neg, LOW);
   digitalWrite(left_rear_pos, LOW);
-
-  // set right speed to 100 through enable pins
-  analogWrite(right_ena_1, 100);
-  analogWrite(right_ena_3, 100);
 
   // move right wheels forward
   digitalWrite(right_front_pos, HIGH);
@@ -162,12 +183,11 @@ void go_forward() {
   digitalWrite(right_rear_pos, LOW);
 }
 
-// move the car backwards
+/*
+* move the car backwards
+*/
 void reverse() {
-  // set left speed to 100 through enable pins
-  analogWrite(left_ena_1, 100);
-  analogWrite(left_ena_3, 100);
-
+  // using the current speed
   // move left wheels backwards
   digitalWrite(left_front_neg, HIGH);
   digitalWrite(left_rear_neg, HIGH);
@@ -175,10 +195,6 @@ void reverse() {
   // ensure left forwards is off
   digitalWrite(left_front_pos, LOW);
   digitalWrite(left_rear_pos, LOW);
-
-  // set right speed to 100 through enable pins
-  analogWrite(right_ena_1, 100);
-  analogWrite(right_ena_3, 100);
 
   // move right wheels backwards
   digitalWrite(right_front_neg, HIGH);
@@ -189,28 +205,30 @@ void reverse() {
   digitalWrite(right_rear_pos, LOW);
 }
 
-// stop the car
-void stop() {
+/*
+* stop the car
+*/
+void stop_motors() {
   // set all enable pins to change speed to 0
-  analogWrite(left_ena_1, 0);
-  analogWrite(left_ena_3, 0);
-  analogWrite(right_ena_1, 0);
-  analogWrite(right_ena_3, 0);
+  set_speed(0);
+
+  // turn all motor pins low
+  digitalWrite(right_front_pos, LOW);
+  digitalWrite(right_front_neg, LOW);
+  digitalWrite(left_front_pos, LOW);
+  digitalWrite(left_front_neg, LOW);
 }
 
-// turn the car left
+/*
+* turn the car left
+*/
 void turn_left() {
-  // stop all left motor activity
-  analogWrite(left_ena_1, 0);
-  analogWrite(left_ena_3, 0);
-  digitalWrite(left_front_pos, LOW);
-  digitalWrite(left_rear_neg, LOW);
-  digitalWrite(left_front_neg, LOW);
-  digitalWrite(left_rear_pos, LOW);
+  // stop all motor activity before turning
+  stop_motors();
 
-  // set right wheel speed to 100 through enable pins
-  analogWrite(right_ena_1, 100);
-  analogWrite(right_ena_3, 100);
+  // set right wheel speed to 50 through enable pins
+  analogWrite(right_ena_1, 50);
+  analogWrite(right_ena_3, 50);
 
   // move right wheels fwd
   digitalWrite(right_front_pos, HIGH);
@@ -219,21 +237,22 @@ void turn_left() {
   // ensure right wheels reverse is off
   digitalWrite(right_front_neg, LOW);
   digitalWrite(right_rear_pos, LOW);
+
+  // stop all motor activity after turning, waiting for next command
+  delay(1000);
+  stop_motors();
 }
 
-// turn the car left
+/*
+* turn the car right
+*/
 void turn_right() {
-  // stop all right motor activity
-  analogWrite(right_ena_1, 0);
-  analogWrite(right_ena_3, 0);
-  digitalWrite(right_front_pos, LOW);
-  digitalWrite(right_rear_neg, LOW);
-  digitalWrite(right_front_neg, LOW);
-  digitalWrite(right_rear_pos, LOW);
+  // stop all motor activity before turning
+  stop_motors();
 
   // set left wheel speed to 100 through enable pins
-  analogWrite(left_ena_1, 100);
-  analogWrite(left_ena_3, 100);
+  analogWrite(left_ena_1, 50);
+  analogWrite(left_ena_3, 50);
 
   // move left wheels fwd
   digitalWrite(left_front_pos, HIGH);
@@ -242,4 +261,8 @@ void turn_right() {
   // ensure left wheels reverse is off
   digitalWrite(left_front_neg, LOW);
   digitalWrite(left_rear_pos, LOW);
+
+  // stop all motor activity after turning, waiting for next command
+  delay(1000);
+  stop_motors();
 }
